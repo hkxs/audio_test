3a00a9620dc4bc45ce669866a3775d1b8c4c13c1
hkxs <lalvarezf25@gmail.com>
2024-10-24 13:56:28 -0500

    Mix microphone with some sin wave

--- src/audio_test/basic_mix.py
+++ src/audio_test/basic_mix.py
@@ -29,11 +29,17 @@ SAMPLE_RATE = 44100
 RECORD_SECONDS = 10
 
 p = pyaudio.PyAudio()
+samples = np.arange(FRAME_SIZE) / SAMPLE_RATE
+
+# (44100 / 1024) = 43.07 we can use 430 to get complete cycles
+sine_wave = 0.1 * np.sin(2 * np.pi * 430 * samples)
+
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=SAMPLE_RATE, input=True, output=True, frames_per_buffer=FRAME_SIZE)
 
-for _ in range(0, int(SAMPLE_RATE // FRAME_SIZE * RECORD_SECONDS)):
+for i in range(0, int((SAMPLE_RATE // FRAME_SIZE) * RECORD_SECONDS)):
     raw_data = stream.read(FRAME_SIZE)
     data = np.frombuffer(raw_data, dtype=np.float32)
+    data = 100 * data + sine_wave
     stream.write(data.T.astype('float32').tobytes())
 
 stream.close()
