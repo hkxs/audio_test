d3f71d11f96c5116e1e7a2a821c67da5d263942f
hkxs <lalvarezf25@gmail.com>
2024-10-28 15:29:30 -0600

    Use different bandwidths to get the best of both worlds

--- src/audio_test/pitch_estimation.py
+++ src/audio_test/pitch_estimation.py
@@ -22,11 +22,24 @@ import logging
 
 import numpy as np
 
-
 logger = logging.getLogger(__name__)
 
 
-def pitch_estimator(signal: np.array, sample_rate: int) -> float:
+def _period_estimator(rxx: np.array, sample_rate: float, bandwidth: tuple[float, float], umbral: float) -> float:
+    min_sample = sample_rate // bandwidth[1]
+    max_sample = sample_rate // bandwidth[0]
+
+    logger.debug(f"{min_sample=}, {max_sample=}")
+    amplitude = rxx[min_sample:max_sample].max()
+    logger.debug(f"Max Energy: {float(rxx[0])}, Harmonic Energy: {float(amplitude)}, Ratio: {float(amplitude/rxx[0])}")
+
+    index = rxx[min_sample:max_sample].argmax(axis=0)
+    period = (index + min_sample) if amplitude >= (umbral * rxx[0]) else 0
+    logger.debug(f"Harmonic Index: {int(period)}")
+    return period
+
+
+def pitch_estimator(signal: np.array, sample_rate: float) -> float:
     """
     Estimate the pitch of a signal
 
@@ -54,24 +67,27 @@ def pitch_estimator(signal: np.array, sample_rate: int) -> float:
     -------
     pitch : float
     """
-    speech_bandwidth = (40, 600)
-    min_sample = sample_rate // speech_bandwidth[1]
-    max_sample  = sample_rate // speech_bandwidth[0]
+    umbral = 0.30
+    speech_bandwidth = (100, 600)
+    max_period = (sample_rate // speech_bandwidth[1])
 
-    logger.debug(f"{min_sample=}, {max_sample=}")
     rxx = np.correlate(signal, signal, mode='full')[len(signal) - 1:]
 
-    amplitude = rxx[min_sample:max_sample].max()
-    logger.debug(f"Max Energy: {float(rxx[0])}, Harmonic Energy: {float(amplitude)}, Ratio: {float(amplitude/rxx[0])}")
-
-    UMBRAL = 0.3
-    index = rxx[min_sample:max_sample].argmax(axis=0)
-    # index = np.where(rxx[min_sample:max_sample] == amplitude)[0]
-    period = (index + min_sample) if amplitude >= (UMBRAL * rxx[0]) else 0
-    logger.debug(f"Harmonic Index: {int(period)}")
+    period = _period_estimator(rxx, sample_rate, speech_bandwidth, umbral)
+    if period <= max_period:
+        # Our signal is so low on frequency that we need to update our interval,
+        # we'll adjust the bandwidth and the umbral
+        umbral = 0.1
+        max_period = (sample_rate // speech_bandwidth[1])
+        speech_bandwidth = (40, 100)
+        period = _period_estimator(rxx, sample_rate, speech_bandwidth, umbral)
+        if period < max_period:
+            # if after the bandwidth adjustment we're still over the limit,
+            # mark it as non-periodic
+            period = 0
+            logger.debug("Non-periodic signal")
 
     freq = sample_rate / period if period else 0
-    logger.debug(f"Freq: {freq}" if freq else "Non periodic Sigal")
 
     return freq
 
@@ -81,61 +97,37 @@ if __name__ == '__main__':
     import random
     from datetime import datetime
 
-    import matplotlib.pyplot as plt
-
-    logging.basicConfig(level=logging.INFO)
-    logger.setLevel(logging.DEBUG)
-
     FRAME_SIZE = 1024
     FS = 44100
     samples = np.arange(FRAME_SIZE) / FS
+    random.seed(datetime.now().timestamp())
 
-    sine_wave = np.sin(2 * np.pi * 90 * samples)
-    est_freq = pitch_estimator(sine_wave, FS)
-    print(est_freq)
+    print("-" * 20)
+    print("Clean Signal")
+    print("-" * 20)
+    for frequency in range(50, 600, 10):
+        sine_wave = np.sin(2 * np.pi * frequency * samples)
+        print(f"Real frequency: {frequency}")
+        est_freq = pitch_estimator(sine_wave, FS)
+        print(f"Estimated frequency: {est_freq}")
 
-    # random.seed(datetime.now().timestamp())
-    #
-    # print("-" * 20)
-    # print("Clean Signal")
-    # print("-" * 20)
-    # for iteration in range(0, 10):
-    #     print(f"{iteration=}")
-    #     frequency = random.randint(40, 600)
-    #     sine_wave = np.sin(2 * np.pi * frequency * samples)
-    #     print(f"Real frequency: {frequency}")
-    #     est_freq = pitch_estimator(sine_wave, FS)
-    #     print(f"Estimated frequency: {est_freq}")
-    #
-    #     error_rate = abs((frequency - est_freq) / frequency)
-    #     error_rate = float(100 * error_rate)
-    #     print(f"{error_rate=}%")
-    #     assert error_rate <= 10  # check if we have less than 10% error
-    #
-    #
-    # print("-" * 20)
-    # print("Noise")
-    # print("-" * 20)
-    # for iteration in range(0, 10):
-    #     print(f"{iteration=}")
-    #     noise = np.random.rand(FRAME_SIZE)
-    #     est_freq = pitch_estimator(noise, FS)
-    #     print(f"Estimated frequency: {est_freq}")
-    #     assert est_freq == 0
-    #
-    # print("-" * 20)
-    # print("Signal + Noise")
-    # print("-" * 20)
-    # for iteration in range(0, 10):
-    #     print(f"{iteration=}")
-    #     frequency = random.randint(40, 600)
-    #     sine_wave = np.sin(2 * np.pi * frequency * samples)
-    #     noise = 0.1 * np.random.rand(FRAME_SIZE)
-    #     print(f"Real frequency: {frequency}")
-    #     est_freq = pitch_estimator(sine_wave + noise, FS)
-    #     print(f"Estimated frequency: {est_freq}")
-    #
-    #     error_rate = abs((frequency - est_freq) / frequency)
-    #     error_rate = float(100 * error_rate)
-    #     print(f"{error_rate=}%")
-    #     assert error_rate <= 10  # check if we have less than 10% error
+        error_rate = abs((frequency - est_freq) / frequency)
+        error_rate = float(100 * error_rate)
+        print(f"{error_rate=}%")
+        assert error_rate <= 10  # check if we have less than 10% error
+
+
+    print("-" * 20)
+    print("Signal + Noise")
+    print("-" * 20)
+    for frequency in range(50, 600, 10):
+        sine_wave = np.sin(2 * np.pi * frequency * samples)
+        noise = 0.5 * np.random.rand(FRAME_SIZE)
+        print(f"Real frequency: {frequency}")
+        est_freq = pitch_estimator(sine_wave + noise, FS)
+        print(f"Estimated frequency: {est_freq}")
+
+        error_rate = abs((frequency - est_freq) / frequency)
+        error_rate = float(100 * error_rate)
+        print(f"{error_rate=}%")
+        assert error_rate <= 15  # check if we have less than 15% error
