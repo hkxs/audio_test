a5aa8b1c5c99330cb23970c480c8e45971de7756
hkxs <lalvarezf25@gmail.com>
2024-10-30 09:57:58 -0600

    Turns out I have the filter coefficients wrong

--- src/audio_test/encoder.py
+++ src/audio_test/encoder.py
@@ -21,6 +21,7 @@ import time
 from pathlib import Path
 
 import librosa
+import numpy as np
 import pyaudio
 import scipy
 
@@ -31,15 +32,16 @@ from pitch_estimation import pitch_estimator
 directory = Path(__file__).parent.absolute()
 audio_dir = directory.parent.parent / 'audios'
 data, sr = librosa.load(audio_dir / "o_sound.wav", mono=False, sr=None)
+data = data / np.max(np.abs(data))
 
 pitch = pitch_estimator(data, sr)
 print(pitch)  # this should be close to 100Hz
-b = librosa.lpc(data, order=10)
+a = librosa.lpc(data, order=10)
 
 period = int(sr // pitch)
 impulse_train = scipy.signal.unit_impulse(len(data), range(1, len(data), period))
-reconstructed = scipy.signal.lfilter(b[1:], [1], impulse_train)
-
+reconstructed = scipy.signal.lfilter([1], a, impulse_train)
+reconstructed = reconstructed / np.max(np.abs(reconstructed))
 
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32, channels=1, rate=int(sr), output=True)
@@ -53,8 +55,10 @@ stream.close()
 p.terminate()
 
 import matplotlib.pyplot as plt
-plt.plot(data)
-plt.show()
-
-plt.plot(reconstructed)
+fig, ax = plt.subplots()
+ax.plot(data[-512:])
+ax.plot(impulse_train[-512:])
+ax.plot(reconstructed[-512:])
+ax.grid(which='both')
+ax.legend(['original', 'impulse train', 'reconstructed'])
 plt.show()
